933cc3532d87f2ca44b525f4d0f2c043ddc6950d
toofaniCoder <smartcoder500@gmail.com>
2021-10-15 01:40:06 -0700

    starter files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <div className="quote">
+        <h4 className="quote__text">
+          You have to grow from the inside out. None can teach you, none can
+          make you spiritual. There is no other teacher but your own soul.
+        </h4>
+        <p className="quote__author">Swami Vivekananda</p>
+        <button className="quote-button">
+          <i class="fas fa-sync"></i>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
